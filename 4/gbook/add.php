c29356091cfc91f0309402656562c71f832e9563
Sergey <cer-c@mail.ru>
2018-07-09 09:32:09 +0300

    Исправления ДЗ №2

--- 4/gbook/add.php
+++ 4/gbook/add.php
@@ -6,9 +6,6 @@ include __DIR__.'/functions.php';
 if (isset($_POST['record']) && !empty($_POST['record'])) {
     $records = dataBook();
 
-    foreach ($records as $key => $record) {
-        $records[$key] = trim($record);
-    }
     $records[] = $_POST['record'];
 
     file_put_contents(__DIR__.'/data.txt', implode("\n", $records));
--- 4/gbook/functions.php
+++ 4/gbook/functions.php
@@ -1,5 +1,5 @@
 <?php
 
 function dataBook() {
-    return file(__DIR__.'/data.txt');
+    return file(__DIR__.'/data.txt', FILE_IGNORE_NEW_LINES);
 }
