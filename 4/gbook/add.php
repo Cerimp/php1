b2beaeee03e89b437b4422fd0c580963a26a813f
Sergey <cer-c@mail.ru>
2018-07-09 09:41:12 +0300

    Исправления №1

--- 4/gbook/add.php
+++ 4/gbook/add.php
@@ -5,7 +5,6 @@ include __DIR__.'/functions.php';
 
 if (isset($_POST['record']) && !empty($_POST['record'])) {
     $records = dataBook();
-
     $records[] = $_POST['record'];
 
     file_put_contents(__DIR__.'/data.txt', implode("\n", $records));
